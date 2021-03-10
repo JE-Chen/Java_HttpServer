c2f7988ceaba0915461565256779de4bef61a210
JE-Chen <zenmailman@gmail.com>
2021-03-10 10:11:11 +0800

    Add CORS
    Add CORS

---
+++ src/main/java/HttpServerHandlerJE.java
@@ -0,0 +1,49 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class HttpServerHandlerJE implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String request = null;
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+            httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+            httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            httpExchange.sendResponseHeaders(204, -1);
+            return;
+        }
+        if ("GET".equals(httpExchange.getRequestMethod())) {
+            request = handleGetRequest(httpExchange);
+        }
+        handleResponse(httpExchange, request);
+    }
+
+    private String handleGetRequest(HttpExchange exchange) {
+        return exchange.getRequestURI()
+                .toString()
+                .split("//?")[1]
+                .split("=")[1];
+    }
+
+    private void handleResponse(HttpExchange httpExchange, String request) throws IOException {
+        OutputStream outputStream = httpExchange.getResponseBody();
+        String htmlResponse = "<html>" +
+                "<head>" +
+                "</head>" +
+                "<body>" +
+                "<h1>" +
+                request +
+                "Hello" +
+                "</h1>" +
+                "</body>" +
+                "</html>";
+        httpExchange.sendResponseHeaders(200, htmlResponse.length());
+        outputStream.write(htmlResponse.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+}
